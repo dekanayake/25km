d4b07a272ef7d007fb1ef357a975a529b1e2858a
Duminda <duminda.ekanayake@reece.com.au>
2020-10-29 16:45:06 +1100

    added changes

--- frontend/src/searchPlaces.js
+++ frontend/src/searchPlaces.js
@@ -4,8 +4,63 @@ import {
     MDCList
 } from '@material/list';
 import * as turf from '@turf/turf';
+import proj4 from 'proj4';
+import {
+    MDCTabBar
+} from '@material/tab-bar';
+import {
+    MDCCheckbox
+} from '@material/checkbox';
+
+
+
+const epsg4326 = require('epsg-index/s/4326.json')
+const epsg3857 = require('epsg-index/s/3857.json');
 
 const axios = require('axios').default;
+const places = []
+let filteredPlaces = []
+const placeMarkers = []
+
+
+const showAddFreinds = () => {
+    document.getElementById('userinput').style.display = 'inline';
+    document.getElementById('mapControl').style.display = 'none'
+    document.getElementById('map').style.visibility = 'hidden'
+}
+
+const showMap = () => {
+    document.getElementById('userinput').style.display = 'none';
+    document.getElementById('mapControl').style.display = 'inline'
+    document.getElementById('map').style.visibility = 'visible'
+}
+
+const showPlaceMarkers = () => {
+    placeMarkers.forEach(marker => {
+        marker.remove()
+    })
+    filteredPlaces.forEach(filteredPlace => {
+        let makrerIcon
+        if (filteredPlace.types.includes('park')) {
+            makrerIcon = '<i class="material-icons nature md-24">nature</i>'
+        } else if (filteredPlace.types.includes('cafe')) {
+            makrerIcon = '<i class="material-icons cafe md-24">local_cafe</i>'
+        } else if (filteredPlace.types.includes('restaurant')) {
+            makrerIcon = '<i class="material-icons restaurant md-24">restaurant</i>'
+        } else {
+            console.log(filteredPlace)
+            makrerIcon = '<i class="material-icons marker md-24">room</i>'
+        }
+        var el = document.createElement('div');
+        el.className = 'geocoder-dropdown-item'
+        el.innerHTML = `${makrerIcon}`
+        let marker = new mapboxgl.Marker(el)
+            .setLngLat(filteredPlace.location.coordinates)
+            .addTo(map);
+        placeMarkers.push(marker)
+    })
+}
+
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHVtaW5kYWUiLCJhIjoiY2tnbGd6NTAzMTA2aTJ0cGQ4Zjl5a29rOSJ9.DWoS_odLf0g8fA2t2_4iBQ';
 
@@ -37,8 +92,63 @@ var geocoderFreind = new MapboxGeocoder({
 
 geocoderFreind.addTo('#geocoderFreind');
 
+map.on('load', function () {
+
+    map.addSource('metroploitan-suburbs', {
+        'type': 'geojson',
+        'data': geojson
+    });
+
+    map.addLayer({
+        'id': 'metroploitan-suburbs',
+        'type': 'fill',
+        'source': 'metroploitan-suburbs',
+        'layout': {},
+        'paint': {
+            'fill-color': '#088',
+            'fill-opacity': 0.8
+        }
+    });
+
+    map.addLayer({
+        'id': 'metroploitan-suburbs-text',
+        'type': 'symbol',
+        'source': 'metroploitan-suburbs',
+        'layout': {
+            'text-field': 'Melbourne metroploitan area',
+            'text-justify': 'center'
+        },
+        'paint': {
+            'text-opacity': 0.6,
+        }
+    });
+
+
+    fetch('/metro.json')
+        .then(response => response.json())
+        .then(data => {
+            data.features[0].geometry.coordinates[0][0] = data.features[0].geometry.coordinates[0][
+                0
+            ].map(coords => {
+                return proj4(epsg3857.proj4, epsg4326.proj4, [coords[0], coords[1]]);
+            })
+
+            var metropolitanGeoJson = {
+                'type': 'FeatureCollection',
+                'features': []
+            };
+            metropolitanGeoJson.features = data.features
+            map.getSource('metroploitan-suburbs').setData(metropolitanGeoJson)
+
+
+        });
+
+
+});
+
 const freindGeoCodeList = []
 let freindGeoResult
+let mapTabAdded
 
 const renderFreindAddressList = () => {
     var freindAddressList = document.getElementById('freindAddressList');
@@ -64,6 +174,38 @@ const findIntersection = () => {
         };
         return turf.circle(center, radius, options);
     });
+
+    circles.forEach((circle, index) => {
+        map.addSource(`${freindGeoCodeList[index].name}-circle`, {
+            'type': 'geojson',
+            'data': circle
+        });
+
+        map.addLayer({
+            "id": `${freindGeoCodeList[index].name}-circle`,
+            "type": "fill",
+            "source": `${freindGeoCodeList[index].name}-circle`,
+            "layout": {},
+            "paint": {
+                "fill-color": "red",
+                "fill-opacity": 0.2,
+            }
+        });
+
+        map.addLayer({
+            'id': `${freindGeoCodeList[index].name}-circle-text`,
+            'type': 'symbol',
+            'source': `${freindGeoCodeList[index].name}-circle`,
+            'layout': {
+                'text-field': `${freindGeoCodeList[index].name}`,
+                'text-justify': 'center'
+            },
+            'paint': {
+                'text-opacity': 0.6,
+            }
+        });
+    })
+
     let intersection
     let intersectionInputs = []
     circles.forEach((circle, index) => {
@@ -76,17 +218,25 @@ const findIntersection = () => {
         }
     })
 
-    console.log(JSON.stringify(intersection))
-    
     axios.post('https://iqrbx1orsa.execute-api.ap-southeast-2.amazonaws.com/prod/places/nearBy', {
-        geometry: intersection.geometry
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+            geometry: intersection.geometry
+        })
+        .then(function (response) {
+
+            const matchedPlaces = response.data
+
+            while (places.length) {
+                A.pop();
+            }
+            matchedPlaces.forEach(responseItem => {
+                places.push(responseItem)
+            })
+            filteredPlaces = places
+            showPlaceMarkers()
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
 }
 
@@ -106,6 +256,41 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.getElementById('showPlaces').addEventListener("click", (event) => {
+        document.getElementById('mapTab').disabled = false;
+        tabBar.activateTab(1)
+
         findIntersection()
     });
+
+    const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+    tabBar.listen("MDCTabBar:activated", event => {
+        console.log(event)
+        const tabIndex = event.detail.index
+        if (tabIndex === 0) {
+            showAddFreinds()
+        }
+        if (tabIndex === 1) {
+            showMap()
+        }
+    })
+
+    const checkBoxes = document.querySelectorAll('.mdc-checkbox')
+    const mdcCheckBoxList = []
+    checkBoxes.forEach(checkBox => {
+        const mdcCheckbox = new MDCCheckbox(checkBox)
+        mdcCheckBoxList.push(mdcCheckbox)
+        mdcCheckbox.listen('click', event => {
+            const selectedPlaceTypes = mdcCheckBoxList.filter(mdcCheckbox => mdcCheckbox.checked).map(mdcCheckbox => mdcCheckbox.value)
+            filteredPlaces = places.filter(place => {
+                let exists = false
+                selectedPlaceTypes.forEach(selectedType => {
+                    if (place.types.includes(selectedType)) {
+                        exists = true
+                    }
+                })
+                return exists
+            })
+            showPlaceMarkers()
+        })
+    })
 }, false);
